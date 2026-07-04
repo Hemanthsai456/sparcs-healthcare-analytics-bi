bf4161b92817a7d72de0c226c450d232c7f3a4f2
Ch.Hemanth Sai <hemanthsai.ch456@gmail.com>
2026-07-04 17:17:34 +0530

    Update load staging SQL

--- SQL/02_staging/03_load_staging.sql
+++ SQL/02_staging/03_load_staging.sql
@@ -1,7 +1,7 @@
 TRUNCATE TABLE staging.sparcs_raw;
 
 COPY staging.sparcs_raw
-FROM 'D:\projects\sparcs-healthcare-analytics-platform\sparcs-healthcare-analytics-platform-2\data\sparcs_2024_raw.csv'
+FROM '..\data\sparcs_20xx_raw.csv'
 DELIMITER ','
 CSV HEADER
 ENCODING 'UTF8';
